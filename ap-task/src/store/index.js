173352459622cf586a90e8df322f4ab63c8243ad
Alex Pilugin <alex.pilugin@microsec.co.uk>
2020-12-16 10:41:16 +0000

    preparation

--- ap-task/src/store/index.js
+++ ap-task/src/store/index.js
@@ -3,9 +3,51 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const devMode = process.env.NODE_ENV === "development";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    vehicles: []
+  },
+  getters: {
+    getVehicles: state => state.vehicles,
+    getVehicleById: state => {
+      const vehicles =
+        state.vehicles.length && state.vehicles.length > 0
+          ? state.vehicles
+          : undefined;
+      function getVehicle(id) {
+        if (vehicles) {
+          const selected = vehicles.find(v => v.id === id); // first found
+          return selected;
+        }
+      }
+      // return a closure function:
+      return getVehicle;
+    }
+  },
+  mutations: {
+    addVehicle(state, info) {
+      state.vehicles.push({
+        id: info.eventInfo.id,
+        events: [info]
+      });
+      if (devMode) {
+        console.log(`mutation: addVehicle: ${info.eventInfo.id}`);
+      }
+    },
+    addEvent(state, payload) {
+      const vehicles = state.vehicles;
+      const selected = vehicles.find(v => v.id === payload.id);
+      selected.events.push(payload.event);
+      if (devMode) {
+        console.log(`mutation: addEvent: ${payload.event.eventType}`);
+      }
+    }
+  },
+  actions: {
+    register: (context, info) => context.commit("addVehicle", info),
+    addEvent: (context, payload) => context.commit("addEvent", payload)
+  },
   modules: {}
 });
--- ap-task/src/views/Home.vue
+++ ap-task/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <h1>Technical Task for Cazana</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+//import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
